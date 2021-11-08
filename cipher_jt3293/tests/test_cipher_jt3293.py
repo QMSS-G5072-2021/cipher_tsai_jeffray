012073e9d638c70971cf85f0be2eead441bf689c
Jeffray Tsai <tsaijeffray@gmail.com>
2021-11-08 00:50:11 -0500

    Package for HW7.

--- cipher_jt3293/tests/test_cipher_jt3293.py
+++ cipher_jt3293/tests/test_cipher_jt3293.py
@@ -1,1 +1,8 @@
 from cipher_jt3293 import cipher_jt3293
+
+def test_singleword_cipher():
+    example = 'hello'
+    number = 2
+    expected = 'jgnnq'
+    actual = cipher(example, number, encrypt=True)
+    assert actual == expected 
